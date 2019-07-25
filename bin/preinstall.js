efa438ac960f398f9ddf7705e83ae96ef5796dc4
Ken <gaokun@nextop.cn>
2019-07-25 16:29:25 +0800

    commit

--- bin/preinstall.js
+++ bin/preinstall.js
@@ -31,23 +31,42 @@ const fs = require('fs');
 
  * */
 
+const errorMsg = '';
+
 try {
   const vueRuntimeEsmFileName = '../vue/dist/vue.runtime.esm.js';
   fs.accessSync(vueRuntimeEsmFileName, fs.constants.R_OK | fs.constants.W_OK);
-  const file = fs.openSync(vueRuntimeEsmFileName, 'rw');
+  const file = fs.openSync(vueRuntimeEsmFileName, 'r+');
   console.log('file found');
   const content = fs.readFileSync(vueRuntimeEsmFileName, {encoding: 'utf8'});
+  if (!content) {
+    throw 'no content';
+  }
   console.log('got file content');
+  const ifChanged = content.indexOf('vue-who-moved-my-cheese');
+  if (ifChanged > 0) {
+    console.log('already changed');
+    return;
+  }
   const defineReactiveFunctionIndex = content.indexOf('function defineReactive');
   if (defineReactiveFunctionIndex > 0) {
     const keyString = 'var dep = new Dep();';
     const keyStringIndex = content.indexOf(keyString, defineReactiveFunctionIndex);
     if (keyStringIndex > 0) {
-      fs.writeSync(file, 'dep.name = key;\n', keyStringIndex, 'utf8');
+      const newContent = content.substring(0, keyStringIndex + keyString.length)
+        + 'dep.name = key; // vue-who-moved-my-cheese'
+        + content.substring(keyStringIndex + keyString.length);
+
+      fs.writeSync(file, newContent);
       console.log('write file succeed');
+    } else {
+      console.error('dose not found keyString');
     }
+  } else {
+    console.error('dose not found defineReaction');
   }
 } catch (err) {
   console.error('Does not detect vue or something wrong');
+  console.error(err);
 }
 
