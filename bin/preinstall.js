5b7fd69074580286276d87de2ab6138f057fd0f7
Ken <gaokun@nextop.cn>
2019-07-25 15:57:08 +0800

    commit

---
+++ bin/preinstall.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+/**
+ *
+ change vue.runtime.esm.js file, add name on Dep
+
+ change:
+
+ ```js
+   function defineReactive$$1 (
+   obj,
+   key,
+   val,
+   customSetter,
+   shallow
+   ) {
+    var dep = new Dep();
+ ```
+ into:
+
+ ```js
+   function defineReactive$$1 (
+   obj,
+   key,
+   val,
+   customSetter,
+   shallow
+   ) {
+    var dep = new Dep();
+    dep.name = key;   // <==== new
+
+ * */
+
+try {
+  const vueRuntimeEsmFileName = '../vue/dist/vue.runtime.esm.js';
+  fs.accessSync(vueRuntimeEsmFileName, fs.constants.R_OK | fs.constants.W_OK);
+  const file = fs.openSync(vueRuntimeEsmFileName, 'rw');
+  const content = fs.readFileSync(vueRuntimeEsmFileName, {encoding: 'utf8'});
+  const defineReactiveFunctionIndex = content.indexOf('function defineReactive');
+  if (defineReactiveFunctionIndex > 0) {
+    const keyString = 'var dep = new Dep();';
+    const keyStringIndex = content.indexOf(keyString, defineReactiveFunctionIndex);
+    if (keyStringIndex > 0) {
+      fs.writeSync(file, 'dep.name = key;\n', keyStringIndex, 'utf8');
+    }
+  }
+} catch (err) {
+  console.error('Does not detect vue or something wrong');
+}
+
