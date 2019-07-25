3adada8b8182a3c835f5b89ad47889f2f749c6b6
Ken <gaokun@nextop.cn>
2019-07-25 16:00:22 +0800

    commit

--- bin/preinstall.js
+++ bin/preinstall.js
@@ -35,13 +35,16 @@ try {
   const vueRuntimeEsmFileName = '../vue/dist/vue.runtime.esm.js';
   fs.accessSync(vueRuntimeEsmFileName, fs.constants.R_OK | fs.constants.W_OK);
   const file = fs.openSync(vueRuntimeEsmFileName, 'rw');
+  console.log('file found');
   const content = fs.readFileSync(vueRuntimeEsmFileName, {encoding: 'utf8'});
+  console.log('got file content');
   const defineReactiveFunctionIndex = content.indexOf('function defineReactive');
   if (defineReactiveFunctionIndex > 0) {
     const keyString = 'var dep = new Dep();';
     const keyStringIndex = content.indexOf(keyString, defineReactiveFunctionIndex);
     if (keyStringIndex > 0) {
       fs.writeSync(file, 'dep.name = key;\n', keyStringIndex, 'utf8');
+      console.log('write file succeed');
     }
   }
 } catch (err) {
